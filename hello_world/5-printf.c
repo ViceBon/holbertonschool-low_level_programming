8a74e94061f9738d35bda517a443aea4278ea083
vicebon <ignaciobonilla2218@gmail.com>
2024-02-15 09:35:23 -0800

    hello_world

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  * main - Programming is
+ *   *
+ *    * Return: Always 0 (Sucess)
+ *     */
+int main(void)
+{
+	        printf("withproper grammar, but the outcome is a piece of art,\n");
+		        return (0);
+}
