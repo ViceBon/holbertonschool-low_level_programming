986aa87e565ed2b95014b227cd497a2d1740104e
vicebon <ignaciobonilla2218@gmail.com>
2024-02-19 09:36:22 -0800

    version

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@
 int main(void)
 {
 	int n;
+	int ld = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	ld = n % 10;
 	if (ld > 5)
 		printf("and is grater than 5\n");
 	else if (ld < 6)
