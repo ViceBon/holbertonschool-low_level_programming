309abc3854cfd9a1e1a511a074ba8ec0c7638cbc
vicebon <ignaciobonilla2218@gmail.com>
2024-02-19 06:09:10 -0800

    variables

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,18 @@
 /*Random number to 0*/
 
 /*betty stile*/
-int main (void)
+int main(void)
 {
 	int n;
 
 	strand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	else (n = 0) 
+	else
 		printf("%d is cero\n", n);
 
-	return (0);	
+	return (0);
 }
