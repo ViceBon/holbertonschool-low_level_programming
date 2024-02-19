05af64bc1e898b8cbc2fecca2385700932aa9474
vicebon <ignaciobonilla2218@gmail.com>
2024-02-19 09:26:27 -0800

    version

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	if (n % 10 > 5)
 		printf("and is grater than 5\n");
 	else if (n % 10 < 6)
-		ptintf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	else
 		printf("and is 0");
 	return (0);
