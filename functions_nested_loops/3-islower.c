a6b1e7d3ab390e8b641919ffa6b6e00776735329
vicebon <ignaciobonilla2218@gmail.com>
2024-02-20 18:03:24 -0800

    loops

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int r;
+	int c;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
+	int _islower(int c)
+	{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+
 	return (0);
 }
