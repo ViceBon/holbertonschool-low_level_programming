08a140e408058bc3c0e58c56425bfc21a9a33669
ViceBon <ignaciobonilla2218@gmail.com>
2024-02-27 13:44:55 +0000

    pointers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ *
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+
+{
+	int len = 0;
+	int a;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (a = len - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void puts2(char *str);
+void print_rev(char *s);
