9661f92570531eab641e7dd76560e6034885f858
ViceBon <ignaciobonilla2218@gmail.com>
2024-02-27 14:40:52 +0000

    pointers

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,24 +3,24 @@
 /**
  *puts2 - read the numbers there and print the even numbers
  *followed by a new line
- * @s: string to print de chars from
+ * @str: string to print de chars from
  *
  */
 
-void puts2(char *s)
+void puts2(char *str)
 {
 	int len, i;
 
 	len = 0;
 
-	while (s[len] != '\0')
+	while (str[len] != '\0')
 	{
 	len++;
 	}
 
 	for (i = 0; i < len; i += 2)
 	{
-	_putchar(s[i]);
+	_putchar(str[i]);
 	}
 
 	_putchar('\n');
